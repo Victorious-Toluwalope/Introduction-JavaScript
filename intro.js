494a0528e23a404f3c5f5a7f44b7c1e177533322
Victorious-Toluwalope <victorioustolu@gmail.com>
2022-06-24 04:44:21 +0100

    First JS

---
+++ intro.js
@@ -0,0 +1,5 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+console.log('Toluwalope Victoria Adeti-Toyo')
+console.log('Height: 165cm');
+console.log('Country: Nigeria');
